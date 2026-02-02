69fd57ba0e0618929a6ceb7d7cd6753335faf3b0
BulbaSprout <pasabozenko3@gmail.com>
2026-02-02 23:34:28 +0200

    please work and thats it

--- BlobsDesktop/Settings.js
+++ BlobsDesktop/Settings.js
@@ -19,7 +19,7 @@ async function mainScreen() {
     //append window and title
     document.body.append(mainScreenDiv);
 
-    document.querySelector("app").style.height = '95%';
+    document.getElementsByClassName("app")[1].style.height = "95%"
 
     await newTab("Wallpapers", '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-wallpaper-icon lucide-wallpaper"><circle cx="8" cy="9" r="2"/><path d="m9 17 6.1-6.1a2 2 0 0 1 2.81.01L22 15V5a2 2 0 0 0-2-2H4a2 2 0 0 0-2 2v10a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2"/><path d="M8 21h8"/><path d="M12 17v4"/></svg>', mainScreenDiv, wallpapersTabLoad)
     await newTab("Customization", '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-settings2-icon lucide-settings-2"><path d="M14 17H5"/><path d="M19 7h-9"/><circle cx="17" cy="17" r="3"/><circle cx="7" cy="7" r="3"/></svg>', mainScreenDiv, customizationTabLoad)
